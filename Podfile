0cea12bb44cff33c9d4302b0054ce8737dff8a5c
JXEE <dirtymb@gmail.com>
2018-01-05 10:58:32 +0800

    @Add Podfile [Reason] Integrate CocoaPod with 'Firebase'.
    @Add GoogleService-Info.plist
    @Update AppDelegate
    [Reason] Firebase integration.
    
    @Update Info.plist
    [Reason] Modified version string.
    
    @Update project setting
    [Reason] Reorganize folder tree.

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'teddybear' do
+  use_frameworks!
+  # Pods for teddybear
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+
+  target 'teddybearTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- teddybear/Controller/LoginViewController.swift
+++ teddybear/Controller/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  teddybear
 //
 //  Created by JLee21 on 2018/1/5.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
